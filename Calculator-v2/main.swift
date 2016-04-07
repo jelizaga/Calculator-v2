2992b8716650ab00d75ec206295601c7b833d47c
Joel Elizaga <jelizaga@uw.edu>
2016-04-07 13:17:09 -0700

    Added addition array op.

--- Calculator-v2/main.swift
+++ Calculator-v2/main.swift
@@ -26,6 +26,8 @@ func help() {
     print("count - Counts the number of inputs received.")
     print("average - Averages all inputs received.")
     print("fact - Calculates the factorial of one input.")
+    print("add all - Calculates the sum of all received numbers.")
+    print("multiply all - Calculates the result of multiplying all received numbers.")
     print("")
     print("=============================================")
     print("")
@@ -66,8 +68,12 @@ func mod(val1 : Int, val2 : Int) -> Int {
 //////////////////////
 
 // Receives an array of Ints; returns the result of adding every single one.
-func addiArray() {
-    
+func addiArray(storage : [Int]) -> Int {
+    var result:Int = 0
+    for num in storage {
+        result += num
+    }
+    return result
 }
 
 // Receives an array of Ints; returns the result of multiplying every single one.
@@ -89,6 +95,7 @@ func perfOp(val1 : Int, val2 : Int) {
 //////////////////
 
 print("Welcome to Calculator-v2.")
+help()
 
 ////////////////
 // CALCULATOR //
@@ -99,7 +106,7 @@ var calculatorOn = true
 while calculatorOn {
     
     // Declaring empty calculator array.
-    var storage = [UInt]()
+    var storage = [Int]()
     // Declaring empty operator.
     var op = ""
     // Declaring boolean for a completed calculation.
@@ -107,10 +114,37 @@ while calculatorOn {
     // Declaring a boolean for a declared operand.
     var opDeclared = false
     
+    print("Calculate:")
+    
     while !complete {
         
+        // Raw response.
+        var response = readLine()
+        // Response converted into an Int.
+        var operand = Int.init(response!)
+        
+        if (response == "quit") {
+            calculatorOn = false
+            print("")
+            break
+        } else if (response == "help") {
+            help()
+        } else if (response == "+" || response == "-" || response == "*" || response == "/" || response == "%") {
+            op = response!
+            opDeclared = true
+        } else if (response == "count") {
+            
+        } else if (response == "average") {
+            
+        } else if (response == "fact") {
+            
+        } else if (response == "add all") {
+            
+        } else if (response == "multiply all") {
+            
+        }
+        
     }
-    
     
 }
 
