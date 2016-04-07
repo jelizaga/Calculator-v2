c8d2dcfc1f9849085b5e00f4e802e2e2235786f9
Joel Elizaga <jelizaga@uw.edu>
2016-04-07 12:51:46 -0700

    Init.

---
+++ Calculator-v2/main.swift
@@ -0,0 +1,106 @@
+// Joel Elizaga
+// INFO 498 D
+// Spring 2016
+
+// A second iteration of the Swift CLI calculator I wrote earlier--except this one's written a little bit better.
+
+import Foundation
+
+//////////
+// HELP //
+//////////
+
+func help() {
+    
+    print("")
+    print("=============================================")
+    print("")
+    print("USEFUL COMMANDS:")
+    print("quit - Quits the calculator.")
+    print("help - Prints this menu again.")
+    print("")
+    print("VALID OPERATORS:")
+    print("+ - * / %")
+    print("")
+    print("OPERATIONS:")
+    print("count - Counts the number of inputs received.")
+    print("average - Averages all inputs received.")
+    print("fact - Calculates the factorial of one input.")
+    print("")
+    print("=============================================")
+    print("")
+    
+}
+
+//////////////////////
+// BASIC OPERATIONS //
+//////////////////////
+
+// Performs addition.
+func addi() {
+    
+}
+
+// Performs subtraction.
+func subt() {
+    
+}
+
+// Performs multiplication.
+func mult() {
+    
+}
+
+// Performs division.
+func divi() {
+    
+}
+
+// Performs mod.
+func mod() {
+    
+}
+
+//////////////////////
+// ARRAY OPERATIONS //
+//////////////////////
+
+// Receives an array of Ints; returns the result of adding every single one.
+func addiArray() {
+    
+}
+
+// Receives an array of Ints; returns the result of multiplying every single one.
+func multiArray() {
+    
+}
+
+///////////////////////
+// PERFORM OPERATION //
+///////////////////////
+
+// Generic "math operation" function that accepts two Ints and passes them to an operation.
+func perfOp(val1 : Int, val2 : Int) {
+    
+}
+
+//////////////////
+// INTRODUCTION //
+//////////////////
+
+print("Welcome to Calculator-v2.")
+
+////////////////
+// CALCULATOR //
+////////////////
+
+var calculatorOn = true;
+
+while (calculatorOn) {
+    
+    var storage = [Int]()
+    var op = ""
+    
+    
+}
+
